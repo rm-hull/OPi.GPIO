97d7a5a6099e0a535f757f599b75cebb99b86abd
Richard Hull <rm_hull@yahoo.co.uk>
2017-03-11 18:30:37 +0000

    Parameterize tests

--- tests/test_sysfs.py
+++ tests/test_sysfs.py
@@ -6,9 +6,10 @@
 """
 Tests for the :py:mod:`OPi.sysfs` module.
 """
+import pytest
 
-from OPi.sysfs import export, unexport, direction, input, output
-from OPi.constants import INPUT, OUTPUT, LOW, HIGH
+from OPi.sysfs import export, unexport, direction, input, output, edge
+from OPi.constants import IN, OUT, LOW, HIGH, NONE, RISING, FALLING, BOTH
 
 
 def test_export(fs):
@@ -25,43 +26,47 @@ def test_unexport(fs):
         assert fp.read() == "26"
 
 
-def test_direction_input(fs):
+@pytest.mark.parametrize("test_input,expected", [
+    (IN, "in"),
+    (OUT, "out"),
+])
+def test_direction(fs, test_input, expected):
     fs.CreateFile("/sys/class/gpio/gpio198/direction")
-    direction(198, INPUT)
+    direction(198, test_input)
     with open("/sys/class/gpio/gpio198/direction") as fp:
-        assert fp.read() == "in"
+        assert fp.read() == expected
 
 
-def test_direction_output(fs):
-    fs.CreateFile("/sys/class/gpio/gpio199/direction")
-    direction(199, OUTPUT)
-    with open("/sys/class/gpio/gpio199/direction") as fp:
-        assert fp.read() == "out"
-
-
-def test_input_low(fs):
+@pytest.mark.parametrize("test_input,expected", [
+    ("0", LOW),
+    ("1", HIGH),
+])
+def test_input(fs, test_input, expected):
     fs.CreateFile("/sys/class/gpio/gpio0/value")
     with open("/sys/class/gpio/gpio0/value", "w") as fp:
-        fp.write("0\n")
-    assert input(0) == LOW
+        fp.write("{0}\n".format(test_input))
+    assert input(0) == expected
 
 
-def test_input_high(fs):
-    fs.CreateFile("/sys/class/gpio/gpio17/value")
-    with open("/sys/class/gpio/gpio17/value", "w") as fp:
-        fp.write("1\n")
-    assert input(17) == HIGH
-
-
-def test_output_low(fs):
-    fs.CreateFile("/sys/class/gpio/gpio21/value")
-    output(21, LOW)
-    with open("/sys/class/gpio/gpio21/value") as fp:
-        assert fp.read() == "0"
-
-
-def test_output_high(fs):
+@pytest.mark.parametrize("test_input,expected", [
+    (LOW, "0"),
+    (HIGH, "1"),
+])
+def test_output(fs, test_input, expected):
     fs.CreateFile("/sys/class/gpio/gpio15/value")
-    output(15, HIGH)
+    output(15, test_input)
     with open("/sys/class/gpio/gpio15/value") as fp:
-        assert fp.read() == "1"
+        assert fp.read() == expected
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    (NONE, "none"),
+    (RISING, "rising"),
+    (FALLING, "falling"),
+    (BOTH, "both"),
+])
+def test_edge(fs, test_input, expected):
+    fs.CreateFile("/sys/class/gpio/gpio5/edge")
+    edge(5, test_input)
+    with open("/sys/class/gpio/gpio5/edge") as fp:
+        assert fp.read() == expected
