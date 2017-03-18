ad1e3c26f1f76e41d498020509c7b5dad053a796
Richard Hull <rm_hull@yahoo.co.uk>
2017-03-18 16:54:52 +0000

    Test to make sure constants cannot be modified

---
+++ tests/test_const.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017 Richard Hull
+# See LICENSE.rst for details.
+
+"""
+Tests for the :py:mod:`OPi.constants` module.
+"""
+
+import pytest
+import OPi.constants as constants
+
+
+def test_constants_cant_be_remapped():
+    with pytest.raises(constants.ConstError) as ex:
+        constants.HIGH = "banana"
+
+    assert str(ex.value) == "Can't rebind const(HIGH)"
+    assert constants.HIGH == 1
