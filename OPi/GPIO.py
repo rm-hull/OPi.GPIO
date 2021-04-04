c6324adf661ff684c3ed04b4374f301ff6932f2c
Fionnoch <64333406+Fionnoch@users.noreply.github.com>
2021-04-05 00:24:42 +0100

    added more documentation

--- OPi/GPIO.py
+++ OPi/GPIO.py
@@ -702,10 +702,22 @@ def cleanup(channel=None):
 class PWM:
 
     # To Do:
-    # 1. Start tracking pwm cases to _exports_pwm
-    # 2. find way to check _exports against _exports_pwm to make sure there is no overlap. Dont think it is a problem for OPi pc+ but better futureproof
+    # 1. Start tracking pwm cases to  list like _exports say _exports_pwm
+    # 2. find way to check _exports against _exports_pwm to make sure there is no overlap.
+    # 3. Create map of pwm pins to various boards.
 
     def __init__(self, chip, pin, frequency, duty_cycle_percent, invert_polarity=False):  # (pwm pin, frequency in KHz)
+
+        """
+        Setup the PWM object to control.
+
+        :param chip: the pwm chip number you wish to use.
+        :param pin: the pwm pin number you wish to use.
+        :param frequency: the frequency of the pwm signal in hertz.
+        :param duty_cycle_percent: the duty cycle percentage.
+        :param invert_polarity: invert the duty cycle.
+            (:py:attr:`True` or :py:attr:`False`).
+        """
 
         self.chip = chip
         self.pin = pin
@@ -731,9 +743,15 @@ class PWM:
                 raise e
 
     def start_pwm(self):  # turn on pwm by setting the duty cycle to what the user specified
+        """
+        Start PWM Signal.
+        """
         return sysfs.PWM_Duty_Cycle_Percent(self.chip, self.pin, self.duty_cycle_percent)  # duty cycle controls the on-off
 
     def stop_pwm(self):  # turn on pwm by setting the duty cycle to 0
+        """
+        Stop PWM Signal.
+        """
         return sysfs.PWM_Duty_Cycle_Percent(self.chip, self.pin, 0)  # duty cycle at 0 is the equivilant of off
 
     def change_frequency(self, new_frequency):
@@ -744,6 +762,12 @@ class PWM:
         # 4. If decreasing update the duty cycle period and then the pwm period
         # Why:
         # The sysfs rule for PWM is that PWM Period >= duty cycle period (in nanosecs)
+
+        """
+        Change the frequency of the signal.
+
+        :param new_frequency: the new PWM frequency.
+        """
 
         pwm_period = (1 / new_frequency) * 1e9
         pwm_period = int(round(pwm_period, 0))
@@ -763,6 +787,12 @@ class PWM:
         self.frequency = new_frequency  # update the frequency
 
     def duty_cycle(self, duty_cycle_percent):  # in percentage (0-100)
+        """
+        Change the duty cycle of the signal.
+
+        :param duty_cycle_percent: the new PWM duty cycle as a percentage.
+        """
+        
         if (0 <= duty_cycle_percent <= 100):
             self.duty_cycle_percent = duty_cycle_percent
             return sysfs.PWM_Duty_Cycle_Percent(self.chip, self.pin, self.duty_cycle_percent)
@@ -770,9 +800,15 @@ class PWM:
             raise Exception("Duty cycle must br between 0 and 100. Current value: {0} is out of bounds".format(duty_cycle_percent))
 
     def pwm_polarity(self):  # invert the polarity of the pwm
+        """
+        Invert the signal.
+        """
         sysfs.PWM_Disable(self.chip, self.pin)
         sysfs.PWM_Polarity(self.chip, self.pin, invert=not(self.invert_polarity))
         sysfs.PWM_Enable(self.chip, self.pin)
 
     def pwm_close(self):
+        """
+        remove the object from the system.
+        """
         sysfs.PWM_Unexport(self.chip, self.pin)
