a6b8e984d1e30e7d4f53c9def625aabe055a4ed4
wtwver <53866088+wtwver@users.noreply.github.com>
2022-11-21 16:11:28 +0800

    fix import

--- orangepi/pi4B.py
+++ orangepi/pi4B.py
@@ -9,19 +9,19 @@ Alternative pin mappings for Orange PI 4B
 Usage:
 
 .. code:: python
-   import orangepi.4B
+   import orangepi.pi4B
    from OPi import GPIO
 
-   GPIO.setmode(orangepi.4B.BOARD) or GPIO.setmode(orangepi.plus4B.BCM)
+   GPIO.setmode(orangepi.pi4B.BOARD) or GPIO.setmode(orangepi.pi4B.BCM)
 """
 
 # pin number = (position of letter in alphabet - 1) * 32 + pin number
 # So, PD14 will be (4 - 1) * 32 + 14 = 110
 
-import orangepi.pi4
+import orangepi.pi4B
 
 # Orange Pi One physical board pin to GPIO pin
-BOARD = orangepi.pi4.BOARD
+BOARD = orangepi.pi4B.BOARD
 
 # Orange Pi One BCM pin to actual GPIO pin
-BCM = orangepi.pi4.BCM
+BCM = orangepi.pi4B.BCM
